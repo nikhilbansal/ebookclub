193a5f3852ede3fa9ffdb4b6ec1d7840c6350889
Priya Dharshini <priya.dharshini@flipkart.com>
2013-09-13 03:38:31 +0530

    add member detail

---
+++ src/main/java/com/flipkart/digital/models/MemberDetail.java
@@ -0,0 +1,35 @@
+package com.flipkart.digital.models;
+
+/**
+ * User: priya.dharshini
+ * Date: 13/09/13
+ */
+public class MemberDetail {
+    public String name;
+    public String role;
+    public String club_name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getClub_name() {
+        return club_name;
+    }
+
+    public void setClub_name(String club_name) {
+        this.club_name = club_name;
+    }
+}
